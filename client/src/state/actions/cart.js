e57f7390dd74a6d5bbda4791aafa6d7faba0e553
ante <ante.kotarac@gmail.com>
2020-06-09 00:36:30 +0200

    client routes update

--- client/src/state/actions/cart.js
+++ client/src/state/actions/cart.js
@@ -1,36 +1,41 @@
-import axios from 'axios';
+import axios from "axios";
 
-import {ADD_ITEM_TO_CART,
-        DELETE_ITEM_FROM_CART,
-        INCREASE_ITEM_QUANTITY_IN_CART,
-        DECREASE_ITEM_QUANTITY_IN_CART,
-        CLEAR_CART,
-        ERROR} from './types';
+import {
+  ADD_ITEM_TO_CART,
+  DELETE_ITEM_FROM_CART,
+  INCREASE_ITEM_QUANTITY_IN_CART,
+  DECREASE_ITEM_QUANTITY_IN_CART,
+  CLEAR_CART,
+  ERROR,
+} from "./types";
 
 export const addItemToCart = (item) => {
-  return dispatch => {
-    dispatch({type: ADD_ITEM_TO_CART, payload: item});
-  }
-}
+  return (dispatch) => {
+    dispatch({ type: ADD_ITEM_TO_CART, payload: item });
+  };
+};
 
 export const deleteItemFromCart = (id) => {
-  return dispatch =>  dispatch({type: DELETE_ITEM_FROM_CART, payload: id});
-}
+  return (dispatch) => dispatch({ type: DELETE_ITEM_FROM_CART, payload: id });
+};
 
 export const increaseItemQuantity = (id) => {
-  return dispatch =>  dispatch({type: INCREASE_ITEM_QUANTITY_IN_CART, payload: id});
-}
+  return (dispatch) =>
+    dispatch({ type: INCREASE_ITEM_QUANTITY_IN_CART, payload: id });
+};
 
 export const decreaseItemQuantity = (id) => {
-  return dispatch =>  dispatch({type: DECREASE_ITEM_QUANTITY_IN_CART, payload: id});
-}
+  return (dispatch) =>
+    dispatch({ type: DECREASE_ITEM_QUANTITY_IN_CART, payload: id });
+};
 
 export const submitCart = (cart) => {
-  return dispatch =>  {
-    axios.post('/api/cart', cart)
-    .then(resposne => {
-      dispatch({type: CLEAR_CART})
-    })
-    .catch(e => dispatch({type: ERROR, payload: e}));
+  return (dispatch) => {
+    axios
+      .post("/api/carts", cart)
+      .then((resposne) => {
+        dispatch({ type: CLEAR_CART });
+      })
+      .catch((e) => dispatch({ type: ERROR, payload: e }));
   };
-}
+};
--- client/src/state/actions/product.js
+++ client/src/state/actions/product.js
@@ -1,60 +1,65 @@
-import {GET_PRODUCTS, PRODUCTS_LOADING, ERROR,
-        ADD_PRODUCT_STARTED, ADD_PRODUCT_SUCESS,
-        ADD_PRODUCT_FAIL,
-        PRODUCT_REMOVE_CLEAR_FLAG} from './types';
-import axios from 'axios';
+import {
+  GET_PRODUCTS,
+  PRODUCTS_LOADING,
+  ERROR,
+  ADD_PRODUCT_STARTED,
+  ADD_PRODUCT_SUCESS,
+  ADD_PRODUCT_FAIL,
+  PRODUCT_REMOVE_CLEAR_FLAG,
+} from "./types";
+import axios from "axios";
 
 export const getProducts = () => {
-  return dispatch => {
-    dispatch({type: PRODUCTS_LOADING});
+  return (dispatch) => {
+    dispatch({ type: PRODUCTS_LOADING });
     getAllProducts(dispatch);
-  }
-}
+  };
+};
 
 export const addNewProduct = (product) => {
-  return dispatch => {
-    dispatch({type: ADD_PRODUCT_STARTED});
+  return (dispatch) => {
+    dispatch({ type: ADD_PRODUCT_STARTED });
     axios
-        .post('/api/product', product)
-        .then(res => {
-            const {error} = res.data;
-            if (error) {
-              dispatch({type: ADD_PRODUCT_FAIL})
-              dispatch({type: ERROR, payload: error})
-            }else {
-              dispatch({type: ADD_PRODUCT_SUCESS, payload: res.data});
-              getAllProducts(dispatch);
-            }
-        })
-        .catch(e => dispatch({type: ERROR, payload: e}));
-  }
-}
+      .post("/api/products", product)
+      .then((res) => {
+        const { error } = res.data;
+        if (error) {
+          dispatch({ type: ADD_PRODUCT_FAIL });
+          dispatch({ type: ERROR, payload: error });
+        } else {
+          dispatch({ type: ADD_PRODUCT_SUCESS, payload: res.data });
+          getAllProducts(dispatch);
+        }
+      })
+      .catch((e) => dispatch({ type: ERROR, payload: e }));
+  };
+};
 
 export const deleteProduct = (id) => {
-  return dispatch => {
+  return (dispatch) => {
     axios
-        .delete(`/api/product/${id}`)
-        .then(res => {
-            const {error} = res.data;
-            if (error) {
-              dispatch({type: ERROR, payload: error})
-            }else {
-              getAllProducts(dispatch);
-            }
-        })
-        .catch(e => dispatch({type: ERROR, payload: e}));
-  }
-}
+      .delete(`/api/products/${id}`)
+      .then((res) => {
+        const { error } = res.data;
+        if (error) {
+          dispatch({ type: ERROR, payload: error });
+        } else {
+          getAllProducts(dispatch);
+        }
+      })
+      .catch((e) => dispatch({ type: ERROR, payload: e }));
+  };
+};
 
 export const removeClearFlag = () => {
-    return dispatch => dispatch({type: PRODUCT_REMOVE_CLEAR_FLAG})
-}
+  return (dispatch) => dispatch({ type: PRODUCT_REMOVE_CLEAR_FLAG });
+};
 
 const getAllProducts = (dispatch) => {
   axios
-      .get('/api/product/getall')
-      .then(res => {
-          dispatch({type: GET_PRODUCTS, payload: res.data});
-      })
-      .catch(e => dispatch({type: ERROR, payload: e}));
-}
+    .get("/api/products")
+    .then((res) => {
+      dispatch({ type: GET_PRODUCTS, payload: res.data });
+    })
+    .catch((e) => dispatch({ type: ERROR, payload: e }));
+};
